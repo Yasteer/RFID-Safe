f26345dc0561cbef1800dbe0cac17f69ba5f59c0
Yasteer Sewpersad <30473531+Yasteer@users.noreply.github.com>
2020-12-01 13:33:53 +0200

    Added RFID variables
    Left out displaying information on the PICC Type. The kit comes with the correct components already so maybe a feature for the future ??

--- RFID_Safe.ino
+++ RFID_Safe.ino
@@ -1,25 +1,51 @@
 // Yasteer Sewpersad RFID Safe
 /* System Components: 
+ *  1x RFID Reader & Key (NFC RFID 13.56Mhz RC522) 
  *  1x Servo
  *  1x Metal Box
  *  1x 3D Printed Latch Mechanism
  */
  
 #include <Servo.h> 
+#include <SPI.h>
+#include <MFRC522.h> // Manually install library.
 
-int LED_Unlocked    = 8;
-int LED_Locked      = 9;
-int LED_AddNewUser  = 10;
+// Operation indicators:
+#define LED_Unlocked    1
+#define LED_Locked      2
+#define LED_AddNewUser  3
 
+// SPI output ports:
+#define RST   9
+#define SS    10
+#define MOSI  11
+#define MISO  12
+#define SCK   13 // Clock.
+
+MFRC522 rfid(SS, RST);
+
+// RFID 
+byte Master_Tag_ID[4] = {0,0,0,0}; // ID number of master tag.
+byte Read_Tag_ID[4];
+
+// Servo
 Servo M1;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  M1.attach(3, 1000, 2000);
+
+  // RFID ID Device comes with a SPI interface.
+  SPI.begin();
+  rfid.PCD_Init();
+
+  // Set servo to pin 5 on the Uno.
+  M1.attach(5, 1000, 2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (!rfid.PICC_IsNewCardPresent())
+  {
+    return; // If no card is detected, keep the safe locked. 
+  }
+  Serial.println("New login attempted with a");
 }
